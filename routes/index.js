0bb57ee1789f726c3dcda073f2c1c681a61d7db3
Walter Hietanen <hietanen@gmail.com>
2017-12-04 23:56:19 +0100

    fixed some typos

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/get-data', function(req, res, next) {
   mongo.connect(url, function(err, db) {
     assert.equal(null, err);
     var cursor = db.collection('user-data').find();
-    cursor.forEach(funcrion(doc, err) {
+    cursor.forEach(function(doc, err) {
       assert.equal(null, err);
       resultArray.push(doc);
     }, function() {
@@ -33,8 +33,8 @@ router.post('/insert', function(req, res, next) {
   };
   mongo.connect(url, function(err, db) {
     assert.equal(null, err);
-    db.collection('user-data').insetOne(item, function(err, result) {
-      assert.equal(null, error);
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
       console.log('Item inserted');
       db.close();
     });
