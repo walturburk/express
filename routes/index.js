095bdd1a6648575b72c51d835d9c5a1df4699caa
Walter Hietanen <hietanen@gmail.com>
2017-12-04 23:36:30 +0100

    mongostuff

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
