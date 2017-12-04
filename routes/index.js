b178984cd63d3f4bb3360f4f3ea76af86af7ce28
Walter Hietanen <hietanen@gmail.com>
2017-12-04 23:49:27 +0100

    db manipulation stuff

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,47 @@ var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Form validation', success: req.session.success, errors: req.session.errors });
-  req.session.errors = null;
-  req.session.success = null;
+  res.render('index');
 });
 
-router.post('/submit', function(req, res, next) {
-  req.check('email', 'Invalid email adress').isEmail();
-  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmpassword);
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(funcrion(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
 
-  var errors = req.validationErrors();
-  if (errors) {
-    req.session.errors = errors;
-    req.session.success = false;
-  } else {
-    req.session.success = true;
-  }
-  res.redirect('/');
+router.post('/insert', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').insetOne(item, function(err, result) {
+      assert.equal(null, error);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+  req.redirect('/');
+});
+
+router.post('/update', function(req, res, next) {
+
+});
+
+router.post('/delete', function(req, res, next) {
+
 });
 
 module.exports = router;
