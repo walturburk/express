42308dc69c841899570527669bf89477e43a3e77
walturburk <hietanen@gmail.com>
2017-12-07 13:24:47 +0100

    /update made manually without mongoose or monk

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
 var url = 'mongodb://localhost:27017/test';
@@ -31,18 +32,40 @@ router.post('/insert', function(req, res, next) {
     content: req.body.content,
     author: req.body.author
   };
+
   mongo.connect(url, function(err, db) {
     assert.equal(null, err);
+
     db.collection('user-data').insertOne(item, function(err, result) {
       assert.equal(null, err);
       console.log('Item inserted');
       db.close();
     });
+
   });
+
   req.redirect('/');
 });
 
 router.post('/update', function(req, res, next) {
+
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  var id = req.body.id;
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    
+    db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+
+  });
 
 });
 
